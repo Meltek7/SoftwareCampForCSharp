cf85064f400b813b1b92cfea7aa721b0ce2ef3ce
Meltem TEKELİ AKDAĞ <48786969+Meltek7@users.noreply.github.com>
2021-04-22 16:34:33 +0300

    A simple dictionary class

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] _key;
+        Value[] _value;
+
+        public MyDictionary()
+        {
+            _key = new Key[0];
+            _value = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            bool isKeyExist = ContainsKey(key);
+            if (!isKeyExist)
+            {
+                Key[] _tempKey = _key;
+                Value[] _tempValue = _value;
+
+                _key = new Key[_key.Length + 1];
+                _value = new Value[_value.Length + 1];
+
+
+                for (int i = 0; i < _tempKey.Length; i++)
+                {
+                    _key[i] = _tempKey[i];
+                }
+                for (int i = 0; i < _tempValue.Length; i++)
+                {
+                    _value[i] = _tempValue[i];
+                }
+
+
+                _key[_key.Length - 1] = key;
+                _value[_value.Length - 1] = value;
+            }
+
+            else
+            {
+                throw new Exception("Key is already exist!");
+            }
+        }
+
+        public bool ContainsKey(Key key)
+        {
+            bool isThere = false;
+            foreach (var item in _key)
+            {
+                if (item.ToString() == key.ToString())
+                {
+                    isThere = true;
+                }
+                else
+                {
+                    isThere = false;
+                }
+            }
+            return isThere;
+        }
+
+        public bool ContainsValue(Value value)
+        {
+            bool isThere = false;
+            foreach (var item in _value)
+            {
+                if (item.ToString() == value.ToString())
+                {
+                    isThere = true;
+                }
+                else
+                {
+                    isThere = false;
+                }
+            }
+            return isThere;
+        }
+        public int KeyLength { get { return _key.Length; } }
+        public int ValueLength { get { return _value.Length; } }
+        public Key[] Keys { get { return _key; } }
+        public Value[] Values { get { return _value; } }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> devices = new MyDictionary<int, string>();
+
+
+            devices.Add(1, "Lara");
+            devices.Add(2, "Omnicon");
+            devices.Add(3, "Aquameter");
+
+
+            Console.WriteLine("Antech'in ölçüm cihazları : ");
+            for (int i = 0; i < devices.KeyLength; i++)
+            {
+                Console.WriteLine("{0} : {1}", devices.Keys[i], devices.Values[i]);
+            }
+
+        }
+    }
+}
