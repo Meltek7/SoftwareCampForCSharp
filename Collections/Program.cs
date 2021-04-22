e7247be83e42d819ba822c2ca8f82f2008f48f2c
Meltem TEKELİ AKDAĞ <48786969+Meltek7@users.noreply.github.com>
2021-04-22 13:22:00 +0300

    22.04.21

---
+++ Collections/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> {"A","B","C" };
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 300;
+            product1.UnitsInStock = 24;
+
+            Product product2 = new Product { Id = 2, CategoryId = 3, ProductName = "Defter", UnitPrice = 25, UnitsInStock = 12 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
